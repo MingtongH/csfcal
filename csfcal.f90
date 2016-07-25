cc7e0137b7d822dd2a38706938aac1dbd7fd3505
mingtongh <mh2242@cornell.edu>
2016-07-25 18:33:56 -0400

    csfcal doesn't run with loop for multiple configs

--- csfcal.f90
+++ csfcal.f90
@@ -1,5 +1,5 @@
 program csfcal
-       use prep, only : i16b, rk, Ldes, Lzdes, Sdes_t2, econfigs, nshell, &
+       use prep, only : i16b, rk, Ldes, Lzdes, Sdes_t2, econfigs, nconfig, nshell, &
             !parameters and variables 
            & read_input, checkSmin, checkSmax, checkILmax, Lmax
             !subroutines and functions
@@ -13,7 +13,7 @@ program csfcal
        contains
  
        subroutine main
-           integer :: totdets, minLp, maxLp, innum, num, i, nbasis, ncsf, nconf, iend, istart
+           integer :: totdets, minLp, maxLp, innum, num, i, nbasis, ncsf, iconf, iend, istart
            integer(i16b), allocatable :: detlist(:, :), inbasis(:, :), basislist(:, :), allbasis(:, :)
            real(rk), allocatable :: incoefs(:), coeflist(:), coeftable(:, :)
            integer, allocatable :: ineposes(:, :, :), eposlist(:, :, :)
@@ -29,36 +29,38 @@ program csfcal
            nbasis = 0
            ncsf = 0
            iend = 0
-           nconf = 1!TODO loop
-           istart = iend + 1
-           iend = istart + nshell(nconf) - 1
-           write(*, *) '%%%%%%%%%%%%%%%%%%%%%%  Assign_shell for Config', nconf, '  %%%%%%%%%%%%%%%%%%%'
-           call assign_shell(0_i16b, 0_i16b, 0, 0, 0, 0,&
-           & Lzdes, Sdes_t2, econfigs(istart:iend, 1:3), 0, totdets, detlist)
-           write(*, *) totdets
-           do i = 1, totdets
-               write(*, '(2B16)') detlist(i, 1:2)
-           enddo
-           do i = 1, totdets
-             write(*, *) 'xxxxxxxxxxxxxxxxxxxxx  Projection of det #', i,'  xxxxxxxxxxxxxxxxxxxx' 
+           iconf = 1!TODO loop
+           do iconf = 1, nconfig 
+               istart = iend + 1
+               iend = istart + nshell(iconf) - 1
+               write(*, *) '%%%%%%%%%%%%%%%%%%%%%%  Assign_shell for Config', iconf, '  %%%%%%%%%%%%%%%%%%%'
+               totdets = 0
+               call assign_shell(0_i16b, 0_i16b, 0, 0, 0, 0,&
+                  & Lzdes, Sdes_t2, econfigs(istart:iend, 1:3), 0, totdets, detlist)
+               write(*, *) totdets
+               do i = 1, totdets
+                   write(*, '(2B16)') detlist(i, 1:2)
+               enddo
+               do i = 1, totdets
+                   write(*, *) 'xxxxxxxxxxxxxxxxxxxxx  Projection of det #', i,'  xxxxxxxxxxxxxxxxxxxx' 
 
-             call initlists(detlist(i, 1:2), inbasis, incoefs, ineposes, iniszeros, innum)
-             minLp = 0 
-             maxLp = Lmax(econfigs(istart:iend, 1:3))
-             call Proj_L(minLp, maxLp, Ldes, &
-               & inbasis, incoefs, ineposes, iniszeros, innum, &
-               & basislist, coeflist, eposlist, iszerolist, num)
+                   call initlists(detlist(i, 1:2), inbasis, incoefs, ineposes, iniszeros, innum)
+                   minLp = 0 
+                   maxLp = Lmax(econfigs(istart:iend, 1:3))
+                   call Proj_L(minLp, maxLp, Ldes, &
+                   & inbasis, incoefs, ineposes, iniszeros, innum, &
+                   & basislist, coeflist, eposlist, iszerolist, num)
 
-             !TODO call Proj_L
+                   !TODO call Proj_L
 
-             call getallsigns(basislist, coeflist, eposlist, iszerolist, num)
-             call collect_csf(basislist, coeflist, iszerolist, num, &
-               & allbasis, coeftable, nbasis, ncsf)
-           enddo
+                   call getallsigns(basislist, coeflist, eposlist, iszerolist, num)
+                   call collect_csf(basislist, coeflist, iszerolist, num, &
+                     & allbasis, coeftable, nbasis, ncsf)
+               enddo !for each det
+           enddo !for each config
 
            call normalizetable(coeftable, nbasis, ncsf)
            write(*, *) coeftable(1:nbasis, 1:ncsf)
-
 
 
 
