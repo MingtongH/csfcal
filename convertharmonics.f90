3ba2a2096bc492d40c6773b0c19a6b240912b0f2
mingtongh <mh2242@cornell.edu>
2017-01-18 16:56:53 -0500

    Start on convertharmonics.f90

---
+++ convertharmonics.f90
@@ -0,0 +1,6 @@
+module convertharmonics
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      !Step 1: create new basis list, with 2 coef columns for each csf
+      !Step 2: calculate coefs for each csf
+      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+end module convertharmonics
