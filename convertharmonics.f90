ba7eb652a90a29b55144cf2d4a2753c7233051f7
mingtongh <mh2242@cornell.edu>
2017-03-20 17:15:15 -0400

    Fix coef normalization in Y2Z_singledet, tested once

--- convertharmonics.f90
+++ convertharmonics.f90
@@ -176,6 +176,11 @@ module convertharmonics
                singledets(i, 4) = 0_i16b
                singlecoefs(i, 1:3) = (/1._rk, 0._rk, 0._rk/)
                singlecoefs(i, 4) = msign*1._rk
+
+               if(msign.eq.0) then
+                   singlecoefs(i, 1) = sqrt(2._rk)
+               endif
+
                write(*, '(4B16)') singledets(i, 1:4)
                write(*, *) singlecoefs(i, 1:4)
                i = i + 1
@@ -194,6 +199,10 @@ module convertharmonics
                singledets(i, 4) = tpminus
                singlecoefs(i, 1:3) = (/1._rk, 0._rk, 0._rk/)
                singlecoefs(i, 4) = msign*1._rk
+               if(msign.eq.0) then
+                   singlecoefs(i, 1) = sqrt(2._rk)
+               endif
+
                write(*, '(4B16)') singledets(i, 1:4)
                write(*, *) singlecoefs(i, 1:4)
                i = i + 1
@@ -418,9 +427,10 @@ module convertharmonics
            !end do j 
 
            write(*, *) 'nzbasis =', nzbasis, 'istart=', istart, 'iend=', iend
+           !Divide all coefs by (sqrt(2))^nelec
            do i = istart, iend
-               zcoefs(i, 1) = zcoefs(i, 1) * realcoef
-               zcoefs(i, 2) = zcoefs(i, 2) * realcoef
+               zcoefs(i, 1) = zcoefs(i, 1) * realcoef / (2**(nelec/2)) / (sqrt(2.)**(mod(nelec, 2)))
+               zcoefs(i, 2) = zcoefs(i, 2) * realcoef / (2**(nelec/2)) / (sqrt(2.)**(mod(nelec, 2)))
            enddo
            write(*, *) '========== Appended the following dets and coefs into the zlist ============'
            do i = istart, iend
